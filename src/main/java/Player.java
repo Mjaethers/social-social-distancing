6e73186bca1ef3385b2271e99e6a5ea1e6c7f378
Harry-Ballsack <wonnyjony@gmail.com>
2020-09-18 23:29:13 +0200

    started implementing player and mate into ringOfFire, needs a good bit of work

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -4,17 +4,18 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Player {
-    private final String playerid;
+    private final User playerid;
     private ArrayList<Player> mates;
     private static ArrayList<Player> players = new ArrayList<Player>();
 
-    Player(String ID){
+    Player(User ID){
         playerid = ID;
         mates  = new ArrayList<Player>();
         players.add(this);
+        System.out.println("new player: " + playerid);
     }
 
-    public String getPlayerId(){
+    public User getPlayerId(){
         return playerid;
     }
 
@@ -33,12 +34,31 @@ public class Player {
         }
     }
 
-    public static Player getPlayerByID(String ID){
+    public String getDrinkText(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(this.playerid.getName());
+        for(Player p : this.mates){
+            stringBuilder.append(", and ")
+                         .append(p.getPlayerId().getName());
+        }
+        stringBuilder.append("drink");
+        return stringBuilder.toString();
+    }
+
+    public static ArrayList<Player> getPlayers(){
+        return players;
+    }
+
+    public static Player getPlayerByID(User ID){
         for(Player p : players){
             if(p.getPlayerId().equals(ID)){
                 return p;
             }
         }
         return null;
+    }
+
+    public static boolean isPlayer(User ID){
+        return (getPlayerByID(ID) != null);
     }
 }
